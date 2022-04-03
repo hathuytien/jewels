19c9aafd9f2f27a6d7551c5a4da94687e5e6d609
hathuytien <thuytienha127@gmail.com>
2022-04-03 20:18:46 +0700

    init
    init

---
+++ js/Script.js
@@ -0,0 +1,32 @@
+﻿
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+//show menu con
+$(".drop").click(function () {
+    if ($(".dropdown-content").css("display") == "none") {
+        $(".dropdown-content").css("display", "block");
+    }
+    else {
+        $(".dropdown-content").css("display", "none");
+    }
+});
+function AnHienMenuTrai() {
+    if ($(".menu_trai").css("display") == "none") {
+        $(".menu_trai").css("display", "block");
+    }
+    else {
+        $(".menu_trai").css("display", "none");
+    }
+}
